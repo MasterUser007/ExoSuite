5158a9b4bad4668cd60cae63cdb875a3cbb5508b
MasterUser007 <you@yourdomain.com>
2025-05-24 22:01:38 -0400

    feat(orchestration): integrate PrimeEngineAI /filter endpoint and add pipeline filter test

---
+++ orchestration/src/pipeline.py
@@ -0,0 +1,24 @@
+﻿import requests
+from typing import List, Dict
+
+SERVICE_URLS = {
+    'primeengineai': 'http://primeengineai:8000',
+    'gpu_sieve': 'http://gpu_sieve:8000',
+    'remainder_analysis': 'http://remainder_analysis:8000',
+    'primality_test': 'http://primality_test:8000',
+    'pavi': 'http://pavi:8080',
+}
+
+def run_pipeline(batch: List[Dict]):
+    # Stage 1: Symbolic Filter via PrimeEngineAI
+    resp = requests.post(
+        f\"{SERVICE_URLS['primeengineai']}/filter\",
+        json=batch
+    )
+    resp.raise_for_status()
+    data = resp.json()
+    passed, filtered = data['passed'], data['filtered']
+
+    # (Subsequent stages go here…)
+
+    return data
